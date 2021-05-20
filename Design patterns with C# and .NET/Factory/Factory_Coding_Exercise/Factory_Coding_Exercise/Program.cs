4f047935a1b743fce06af525c1a05b933218888b
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-05-20 23:44:21 +0530

    factory pattern sample - coding exercise

---
+++ Design patterns with C# and .NET/Factory/Factory_Coding_Exercise/Factory_Coding_Exercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Coding_Exercise
+{
+    public class Person
+    {
+        public int Id;
+        public string Name;
+
+        public override string ToString() => $"Id : {Id} Name : {Name}";
+    }
+
+    public class PersonFactory
+    {
+        private static int _lastPersonId = -1;
+        public Person CreatePerson(string name)
+        {
+            return new Person(){Id = ++_lastPersonId, Name = name};
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var personFactory = new PersonFactory();
+
+            var person1 = personFactory.CreatePerson("John");
+            Console.WriteLine(person1);
+
+            var person2 = personFactory.CreatePerson("Harry");
+            Console.WriteLine(person2);
+
+            var person3 = personFactory.CreatePerson("Michael");
+            Console.WriteLine(person3);
+
+            var person4 = personFactory.CreatePerson("James");
+            Console.WriteLine(person4);
+
+            Console.ReadLine();
+        }
+    }
+}
