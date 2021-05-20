62b97b732063ef7821124a50ccf3dfa4bcb65e38
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-05-20 22:52:39 +0530

    factory pattern sample - 3 & 4

---
+++ Design patterns with C# and .NET/Factory/Factory_Four/Factory_Four/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Four
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This is tea is awesome.");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This Coffee is good");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"{amount}ml Tea is being prepared...");
+            return new Tea();
+        }
+    }
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"{amount}ml Coffee is being prepared...");
+            return new Coffee();
+        }
+    }
+
+    // If a factory for a new type of drink is added then the Enum in the below class needs to be altered
+    // This is a clear violation of Open-Close principle
+    // Refer Factory_Four project for the new implementation.
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrinks
+        {
+            Coffee,
+            Tea
+        }
+
+        private Dictionary<AvailableDrinks, IHotDrinkFactory> _factory =
+            new Dictionary<AvailableDrinks, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            foreach (AvailableDrinks drink in Enum.GetValues(typeof(AvailableDrinks)))
+            {
+                var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType("Factory_Four." + Enum.GetName(typeof(AvailableDrinks), drink) + "Factory"));
+                _factory.Add(drink, factory);
+            }
+        }
+
+        public IHotDrink Prepare(AvailableDrinks drink, int amount)
+        {
+            return _factory[drink].Prepare(amount);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+            machine.Prepare(HotDrinkMachine.AvailableDrinks.Coffee, 100).Consume();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Design patterns with C# and .NET/Factory/Factory_Three/Factory_Three/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Three
+{
+    // Instead of having all the factory methods in the main class we could have a separate class for the factory methods, but to
+    // achieve this the Point constructor needs to be made public.
+    // => If Point constructor is made public the whole idea of creating factories is not achieved.
+    // => If you are creating a library then the Point constructor can be made internal
+    // => to hide the constructor for everyone, the factory class is implemented inside the Point class as inner class
+    // This is called the Inner factory pattern.
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Point({x}, {y})";
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Point newCartesianPoint = Point.Factory.NewCartesianPoint(3, 5);
+
+            Console.WriteLine(newCartesianPoint);
+
+            Console.ReadLine();
+        }
+    }
+}
