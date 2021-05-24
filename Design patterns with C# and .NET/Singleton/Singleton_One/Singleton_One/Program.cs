e1324ce6df51c120cb97f58f6f77739ba9d6335e
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-05-24 23:05:19 +0530

    singleton sample project

---
+++ Design patterns with C# and .NET/Singleton/Singleton_One/Singleton_One/Program.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using System;
+
+namespace Singleton_One
+{
+    public class Foo
+    {
+        public EventBroker Broker;
+
+        public Foo(EventBroker broker)
+        {
+            Broker = broker ?? throw new ArgumentNullException(paramName: nameof(broker));
+        }
+    }
+
+    public class EventBroker
+    {
+
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EventBroker>().SingleInstance();
+            builder.RegisterType<Foo>();
+
+            var c = builder.Build();
+
+            var foo1 = c.Resolve<Foo>();
+            var foo2 = c.Resolve<Foo>();
+
+            Console.WriteLine(ReferenceEquals(foo1, foo2));
+            Console.WriteLine(ReferenceEquals(foo1.Broker, foo2.Broker));
+        }
+    }
+}
