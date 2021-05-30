85bf550504be79fbc9275a37d804d3b714e9171c
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-05-30 15:25:47 +0530

    json and xml configuration

---
+++ Dependency Injection and Inversion of Control/DI-IoC-Configuration/JSONConfiguration/JSONConfiguration/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace JSONConfiguration
+{
+    public interface IOperation
+    {
+        float Calculate(float a, float b);
+    }
+
+    public class Addition : IOperation
+    {
+        public float Calculate(float a, float b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Multiplication : IOperation
+    {
+        public float Calculate(float a, float b)
+        {
+            return a * b;
+        }
+    }
+
+    public class CalculationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Addition>().As<IOperation>();
+            builder.RegisterType<Multiplication>().As<IOperation>();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                //.AddJsonFile("config.json");
+                .AddXmlFile("config.xml");
+            var config = configurationBuilder.Build();
+
+            var builder = new ContainerBuilder();
+            var configModule = new ConfigurationModule(config);
+
+            builder.RegisterModule(configModule);
+
+            var container = builder.Build();
+
+            foreach (var op in container.Resolve<IList<IOperation>>())
+            {
+                float a = 100, b = 54;
+                Console.WriteLine($"{op.GetType().Name} of {a} and {b} = {op.Calculate(a, b)}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dependency Injection and Inversion of Control/DI-IoC-Configuration/Modules/Modules/Program.cs
@@ -0,0 +1,77 @@
+﻿using Autofac;
+using System;
+
+namespace Modules
+{
+    public interface IVehicle
+    {
+        void Go();
+    }
+
+    public class Truck : IVehicle
+    {
+        private readonly IDriver _driver;
+
+        public Truck(IDriver driver)
+        {
+            this._driver = driver;
+        }
+
+        public void Go()
+        {
+            _driver.Drive();
+        }
+    }
+
+    public interface IDriver
+    {
+        void Drive();
+    }
+
+    public class CrazyDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Going fast, probably crash and kill people.");
+        }
+    }
+
+    public class SaneDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Going slowly towards the destination.");
+        }
+    }
+
+    public class TransportModule : Module
+    {
+        public bool ObeySpeedLimit { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (ObeySpeedLimit)
+                builder.RegisterType<SaneDriver>().As<IDriver>();
+            else
+                builder.RegisterType<CrazyDriver>().As<IDriver>();
+            builder.RegisterType<Truck>().As<IVehicle>();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TransportModule());
+
+            using (var container = builder.Build())
+            {
+                var vehicle = container.Resolve<IVehicle>();
+                vehicle.Go();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
