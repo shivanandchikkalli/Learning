c695a1c20ae208898bf4ca560c89c1deb587f32b
Shivanand Chikkalli <sychikkalli@gmail.com>
2021-06-22 22:56:18 +0530

    parallerLINQ

---
+++ C# and .NET Concepts/multithreading and parallelization/ParallelLINQ/AsParallel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsParallel
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var items = Enumerable.Range(1, 1000);
+
+            items.AsParallel().ForAll(x =>
+            {
+                Console.WriteLine($"{x} - Thread {Task.CurrentId}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
